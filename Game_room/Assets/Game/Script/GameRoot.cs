2043d352da4af677527ea4ce006baa2c8425bf83
Jeiji7 <egorpopelyuk43@gmail.com>
2023-08-18 02:39:28 +0300

    Add Tilemap number 1
    Такс, добавил тайлмап, я его дополню потом, благо понял как это делать, добавил в спрайт рендере плейера слой чтоб он был на земле, а не за землёй, пока всё

--- Game_room/Assets/Game/Script/GameRoot.cs
+++ Game_room/Assets/Game/Script/GameRoot.cs
@@ -4,11 +4,11 @@ using Shooting;
 internal class GameRoot : MonoBehaviour
 {
     [SerializeField] private MovementInput _movementInput;
-    [SerializeField] private ShootingInput _shootingInput;
+    //[SerializeField] private ShootingInput _shootingInput;
 
     private void Start()
     {
         _movementInput.Activate();
-        _shootingInput.Activate();
+        //_shootingInput.Activate();
     }
 }
